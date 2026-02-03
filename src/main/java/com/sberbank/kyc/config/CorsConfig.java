91edd80a33f1bce96d06244e28cacba64150a82a
supholo <supholo.kar@gmail.com>
2026-02-03 15:33:35 +0530

    Updated CORS setting

--- src/main/java/com/sberbank/kyc/config/CorsConfig.java
+++ src/main/java/com/sberbank/kyc/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:3000",
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://offline-kya.onhercules.app"
                 )
                 .allowedMethods(
                         "GET", "POST", "PUT", "DELETE", "OPTIONS"
